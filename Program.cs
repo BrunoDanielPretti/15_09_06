7cc85d843b2eed9c7edb2de409161caf52a13ccf
MarinaMeza <kaiodelnorte@yahoo.com.ar>
2015-09-08 11:17:41 -0300

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15_09_06
+{
+    /// <summary>
+    /// Tomando como punto de partida a los ejemplos vistos:
+    /// Sobrecargar: 
+    /// -, *, / (Para Metro y Centímetro)
+    /// ==, !=, <=, >= (para Metro y Centímetro)
+    /// Poder asignar un objeto de tipo Metro a uno de tipo Centímetro (y viceversa).
+    /// Realizar desde el Main, todas las operaciones aritméticas, operaciones de comparación y asignación. 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
